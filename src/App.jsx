4ba7709d46552ee9e9a9a98c6d539888cc2080c3
Mohammad Rehan <mohdrehan9122@gmail.com>
2025-07-23 18:50:11 +0530

    Login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout"; // 👈 New layout component
+import Login from "./components/Login";
+import Browse from "./components/Browse";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />, // 👈 Shared layout
+      children: [
+        {
+          path: "/",
+          element: <Login />,
+        },
+        {
+          path: "/browse",
+          element: <Browse />,
+        },
+      ],
+    },
+  ]);
 
-  return (
-    <>
-      <p className="bg-amber-300">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+function Header() {
+  return (
+    <div className="h-auto px-20 py-8 absolute z-40">
+        <img className="h-14" src="https://cdn.freebiesupply.com/logos/large/2x/netflix-2-logo-png-transparent.png" alt="" />
+    </div>
+  )
+}
+
+export default Header
